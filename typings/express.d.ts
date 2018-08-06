84211d085d64062fbab7c72a8a91453bae6be00f
igorVladimirovich <igorvladimirovichbobir@gmail.com>
2018-08-06 09:18:34 +0300

    Update express.d.ts

--- typings/express.d.ts
+++ typings/express.d.ts
@@ -1,5 +1,10 @@
 import {RecaptchaResponse} from '../src/interfaces';
 
+export interface RecaptchaResponse {
+    error?: string,
+    data?: any;
+}
+
 declare global {
     namespace Express {
         export interface Request {
